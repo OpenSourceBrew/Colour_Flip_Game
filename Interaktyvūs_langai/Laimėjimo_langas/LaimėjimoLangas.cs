8adfd26c5bae3f9f92b658cdeee903e1ee090531
Marijus <marijusdomskis@gmail.com>
2025-04-09 23:12:14 +0300

    Bandoma sukurti laimėjimo langą.

---
+++ Interaktyvūs_langai/Laimėjimo_langas/LaimėjimoLangas.cs
@@ -0,0 +1,52 @@
+using Godot;
+using System;
+
+public partial class LaimėjimoLangas : Control
+{
+	private AnimationPlayer animationPlayer;
+	public override void _Ready()
+	{
+		animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer1");
+		if (animationPlayer != null)
+		{
+			animationPlayer.Play("RESET");
+		}
+	}
+
+	public void Resume()
+	{
+		if (!GetTree().Paused)
+			return; // Jei jau atnaujinta, nieko nedarome
+			
+		GetTree().Paused = false;
+
+		if (animationPlayer != null)
+		{
+			animationPlayer.PlayBackwards("blur");
+		}
+		else
+		{
+			GD.Print("Warning: AnimationPlayer not found!");
+		}
+	}
+
+	public void Pause()
+	{
+		GetTree().Paused = true;
+		if (animationPlayer != null)
+		{
+			animationPlayer.Play("blur");
+		}
+	}
+
+	private void _on_grizti_i_meniu_pressed()
+	{
+		GetTree().Paused = false;
+		GetTree().ChangeSceneToFile("res://Interaktyvūs_langai/Pradžios_langas/pradžios_langas.tscn");
+	}
+
+	public override void _Process(double delta)
+	{
+		
+	}
+}
