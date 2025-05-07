59fb0430aba56c0493fd9f692da4912eba06f699
Ema Nickute <114803991+EmaNic1@users.noreply.github.com>
2025-05-07 20:46:36 +0300

    Sutvarkyti keli dalykai
    Idejau, kad resetintu gyvybes is pauzes lango

--- Interaktyvūs_langai/Pauzės_langas/PauzėsLangas.cs
+++ Interaktyvūs_langai/Pauzės_langas/PauzėsLangas.cs
@@ -59,12 +59,18 @@ public partial class PauzėsLangas : Control
 
 	private void _on_pradeti_is_naujo_pressed()
 	{
+		var global = (GlobalState)GetNode("/root/GlobalState");
+		global.ResetGameState();
+		
 		Resume();
 		GetTree().ReloadCurrentScene();
 	}
 
 	private void _on_grizti_i_meniu_pressed()
 	{
+		var global = (GlobalState)GetNode("/root/GlobalState");
+		global.ResetGameState();
+		
 		GetTree().Paused = false;
 		GetTree().ChangeSceneToFile("res://Interaktyvūs_langai/Pradžios_langas/pradžios_langas.tscn");
 	}
